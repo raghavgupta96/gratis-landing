4121df975ea4b19a8e8168a72f13a51581da212a
Raghav Gupta <raghav.gupta01@sjsu.edu>
2018-01-24 14:27:18 -0800

    Updated team and countdown

--- public/index_files/countdown.js
+++ public/index_files/countdown.js
@@ -1,6 +1,6 @@
 
           // Set the date we're counting down to
-          var countDownDate = new Date("Nov 29, 2017 12:00:00").getTime();
+          var countDownDate = new Date("Feb 27, 2018 12:00:00").getTime();
 
           // Update the count down every 1 second
           var x = setInterval(function () {
