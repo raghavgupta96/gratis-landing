74be64f396de007e4bcd6b158fea76b906a7eff5
Daanyaal Ahad Saeed <daanyaal.saeed@sjsu.edu>
2019-02-08 19:34:20 -0800

    Add countdown timer. Date set to Feb 28 12:00

--- public/index_files/countdown.js
+++ public/index_files/countdown.js
@@ -1,15 +1,16 @@
 
           // Set the date we're counting down to
-          var countDownDate = new Date("March 23, 2018 12:00:00").getTime();
+          var countDownDate = new Date("February 8, 2019 19:33:00").getTime();
 
           // Update the count down every 1 second
-          var x = setInterval(function () {
+          window.setInterval(function () {
 
-            // Get todays date and time
-            var now = new Date().getTime();
+             // Get todays date and time
+          var now = new Date().getTime();
 
             // Find the distance between now an the count down date
             var distance = countDownDate - now;
+            console.log(distance)
 
             // Time calculations for days, hours, minutes and seconds
             var days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -18,12 +19,14 @@
             var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
             // Output the result in an element with id="demo"
-            document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
+            document.getElementById("countdown").innerHTML = days + "d " + hours + "h " +
               minutes + "m " + seconds + "s ";
 
             // If the count down is over, write some text
             if (distance < 0) {
-              clearInterval(x);
-              document.getElementById("demo").innerHTML = "EXPIRED";
+              clearInterval(window);
+              document.getElementById("countdown").style.display = "none"
+              document.getElementById("countdown_expire").style.display = "inline";
+              document.getElementById("countdown_expire").innerHTML = `Gratis beta is launched!`;
             }
           }, 1000);
