ee8a7028104009241a5bfef40bfd175042a1ffce
Daanyaal Ahad Saeed <daanyaal.saeed@sjsu.edu>
2019-02-25 00:19:44 -0800

    fixed Responsive JS file

--- public/index_files/resources/js/queries.js
+++ public/index_files/resources/js/queries.js
@@ -1,11 +1,14 @@
 /* adjust Hero image with width*/
 var initWidth = 1281;
-var docWidth = window.innerWidth;
 var image = document.querySelector(".image0");
 
-if (docWidth >= initWidth) {
-    console.log("we are getting bigger");
-    var diff = (docWidth - initWidth) / 100;
+window.addEventListener("resize", function () {
+    var docWidth = window.innerWidth;
 
-    image.style.top = -1 * (55 + diff * 16) + "%";
-}
+    if (docWidth >= initWidth) {
+        var diff = (docWidth - initWidth) / 100;
+        image.style.top = -1 * (55 + diff * 16) + "%";
+    } else {
+        image.style.top = "-290px";
+    }
+});
